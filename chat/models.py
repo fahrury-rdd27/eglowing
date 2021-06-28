5c3bb6b1c09a73d822b306ac0bc39c4a76132ee0
fahrury-rdd27 <fahrury_rdd27@apps.ipb.ac.id>
2021-06-29 06:13:21 +0700

    perubahan admin, migrasi model, routing, dan view

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=1000, blank=False)
+    
+    # def __str__(self):
+    #     return self.name
+
+class Message(models.Model):
+    msg = models.CharField(max_length=300)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
